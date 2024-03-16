7817686f17f618b2db4cb4624c01d7e530ff1650
Aishwarya <76867908+aishwaryabit1603@users.noreply.github.com>
2024-03-16 22:24:38 +0530

    Time: 15 ms (69.17%), Space: 21.7 MB (35.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,22 +5,17 @@ public:
         {
             int i = 0;
             int j = 0;
-            while(i < nums.size()){
-                if(nums[i] == 0) break;
-                i++;
-            }
+            // while(i < nums.size()){
+            //     if(nums[i] == 0) break;
+            //     i++;
+            // }
             while(j < nums.size() && i < nums.size())
             {
                 if(nums[i] != 0){
-                    i++;
-                    continue;
-                }
-                else{
-                    if(i < j && nums[j] != 0){
-                        swap(nums[i],nums[j]);
-                    }
+                    swap(nums[i],nums[j]);
                     j++;
                 }
+                i++;
             }
         }
     }
