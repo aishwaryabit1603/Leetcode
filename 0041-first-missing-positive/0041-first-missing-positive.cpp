36325cfd054fa0dbb7a64a102cfa5458086cd787
Aishwarya <76867908+aishwaryabit1603@users.noreply.github.com>
2024-04-01 21:05:44 +0530

    Time: 140 ms (5.14%), Space: 60.7 MB (6.07%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,31 +2,16 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums)
     {
-        int ans = 0;
-        sort(nums.begin(),nums.end());
+        map<int,bool> mp;
+        for(int i = 0;i < nums.size();i++){
+            if(nums[i] > 0){
+                 mp[nums[i]] = true;
+            }
+        }
         int k = 1;
-        int i;
-        for(i = 0;i < nums.size(); i++){
-            if(nums[i] > 0) break;
-        }
-        int m = i;
-        int n = i+1;
-        while(n < nums.size() && m < nums.size()){
-            if(nums[m] == nums[n]){
-                nums[n] = INT_MIN;
-            }
-            else{
-                m = n;
-            }
-            n++;
-        }
-        while(i < nums.size()){
-            if(nums[i] == INT_MIN){
-                i++;
-                continue;
-            }
-            if(nums[i] != k) break;
-            i++;
+        for(int i = 0;i < nums.size(); i++){
+            if(nums[i] <= 0) continue;
+            if(mp[k] != true) break;
             k++;
         }
         return k;
