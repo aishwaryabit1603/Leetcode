e95a29b623e9bd9cb867ccdbaf526eb11ba85df6
Aishwarya <76867908+aishwaryabit1603@users.noreply.github.com>
2024-04-06 13:27:28 +0530

    Time: 28 ms (19.03%), Space: 13.5 MB (33.60%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<char> s1;
+        string ans = "";
+        for(int i = 0;i < s.length(); i++)
+        {
+            if(s[i] >= 'a' && s[i] <= 'z'){
+                ans += s[i];
+            }
+            else if(s[i] == '('){
+                ans += '(';
+                s1.push('(');
+            }
+            else if(s[i] == ')' && !s1.empty() && s1.top() == '('){
+                ans += ')';
+                s1.pop();
+            }
+        }
+        cout << ans << endl;
+        if(!s1.empty()){
+            int len = s1.size();
+            if(ans.length() == len) return "";
+            
+            for(int i = ans.length()-1;len > 0 && i > -1;i--){
+                if(ans[i] == '('){
+                    ans.erase(i,1);    
+                    len--;
+                }
+            }
+            
+        }
+        return ans;
+    }
+};
