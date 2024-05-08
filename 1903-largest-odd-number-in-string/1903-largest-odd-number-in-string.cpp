db27abe7572e14668eadfd4470300222ea73a905
Aishwarya <76867908+aishwaryabit1603@users.noreply.github.com>
2024-05-09 00:05:54 +0530

    Time: 34 ms (14.98%), Space: 17.9 MB (6.94%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -5,13 +5,11 @@ public:
         int n = num.length();
         int i;
         for(i = n-1;i >= 0; i--){
-            // cout << i << endl;
             int k = num[i]-'0';
             if(k % 2 != 0){
                 break;
             }
         }
-        // cout << i << endl;
         for(int j = 0;j <= i;j++){
             ans += num[j];
         }
