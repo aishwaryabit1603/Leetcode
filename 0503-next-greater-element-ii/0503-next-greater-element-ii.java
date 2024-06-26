fa4b21c1480b97fa6a907c0e63bede849e95938e
Aishwarya <76867908+aishwaryabit1603@users.noreply.github.com>
2024-06-26 23:42:19 +0530

    Time: 16 ms (58.14%), Space: 46.2 MB (39.70%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,49 +4,16 @@ class Solution {
         int [] ans = new int[n];
         Stack<Integer> st = new Stack<>();
         
-        int max = 0;
-        for(int i = 1;i < n ; i++){
-            if(nums[max] < nums[i]) max = i;
-        }
-
-        st.push(nums[n-1]);
-        int i = n-1;
-        int k = 0;
-        while(i > max){
-            if(i != n-1 && nums[i] < nums[i+1]){
-                ans[i] = nums[i+1];
-            }
-            else{
-                while(!st.empty() && st.peek() <= nums[i]) st.pop();
-                if(!st.empty()) ans[i] = st.peek();
-                else{
-                    k = 0;
-                    while(k < i){
-                        if(nums[k] > nums[i]){
-                            break;
-                        }
-                        k++;
-                    }
-                    if(k == i) ans[i] = -1;
-                    else ans[i] = nums[k];
-                }
-            }
+        for(int i = n - 1;i > -1; i--){
+            while(!st.empty() && st.peek() <= nums[i]) st.pop();
             st.push(nums[i]);
-            i--;
         }
-        ans[max] = -1;
-        st.clear();
-        st.push(nums[max]);
-        i--;
-        while(i >= 0){
-            if(nums[i] < nums[i+1]){
-                ans[i] = nums[i+1];
-            }
-            else{
-                while(!st.empty() && st.peek() <= nums[i]) st.pop();
-                if(st.empty()) ans[i] = -1;
-                else ans[i] = st.peek();
-            }
+        int i = n-1;
+        while(i > -1){
+            while(!st.empty() && st.peek() <= nums[i]) st.pop();
+            if(st.empty()) ans[i] = -1;
+            else ans[i] = st.peek();
+            
             st.push(nums[i]);
             i--;
         }
